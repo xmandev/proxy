1ac85d0c5d2874472a754635909190b0c9bda805
xmandev <81670571+xmandev@users.noreply.github.com>
2021-03-31 13:46:44 +0800

    Add files via upload

---
+++ squid_centos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# <UDF name="squid_user" Label="Proxy Username" />
+# <UDF name="squid_password" Label="Proxy Password" />
+# Squid Proxy Server
+# Author: admin@hostonnet.com
+# Blog: https://blog.hostonnet.com
+# Edits: Khaled AlHashem
+# Site: https://knaved.com
+# Version 0.1
+
+squid_user=xxx111
+squid_password=xxx111
+
+yum -y install squid httpd-tools wget openssl
+
+htpasswd -b -c /etc/squid/passwd $squid_user $squid_password
+
+mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
+touch /etc/squid/blacklist.acl
+wget --no-check-certificate --no-cache --no-cookies -O /etc/squid/squid.conf  https://raw.githubusercontent.com/xmandev/proxy/main/squid_centos.conf
+
+firewall-cmd --zone=public --add-port=8088/tcp --permanent
+firewall-cmd --reload
+
+squid -k parse
+
+systemctl restart squid
+systemctl enable squid
+systemctl status squid
+#update-rc.d squid defaults
